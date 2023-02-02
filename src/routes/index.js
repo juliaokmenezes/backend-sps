45ff4f14c71b1a44d83b0d87faa2473fe73dc5db
juliaokmenezes <julia.menezes@ccc.ufcg.edu.br>
2023-02-02 14:52:56 -0300

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.use('/ongs', require('./ongs'));
+router.use("/ongs", require("./ongs"));
 
-router.use('/empresas/', require('./empresas'));
+router.use("/empresas/", require("./empresas"));
 router.use("/admin/", require("./admin"));
-router.use('/posts/', require('./posts'));
-router.use('/produto/', require('./produto'));
+router.use("/posts/", require("./posts"));
+router.use("/produto/", require("./produto"));
+
+router.get("/", (req, res) => {
+  res.status(200).json("Olá! Bem vindo SPS");
+});
 
 module.exports = router;
